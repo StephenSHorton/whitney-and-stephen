30169a9aee0ddbb30325d55649f682662b39bd2c
StephenSHorton <4stephenhorton@gmail.com>
2021-09-29 00:18:00 -0600

    added threejs

--- src/components/navigation/Navbar.tsx
+++ src/components/navigation/Navbar.tsx
@@ -5,7 +5,7 @@ const Navbar: FC = () => {
   return (
     <nav className="fixed top-0 z-40 flex justify-center w-full bg-white shadow-md center-content">
       <div className="flex items-center justify-between w-full p-4">
-        <h1 className="text-lg font-bold">Website Title</h1>
+        <h1 className="text-lg font-bold">Stephen & Whitney</h1>
         <div className="flex justify-end space-x-5">
           <NavLink
             exact
--- src/pages/landing/landing.tsx
+++ src/pages/landing/landing.tsx
@@ -1,14 +1,45 @@
-import React, { FC } from "react";
-import Loading from "components/shared/Loading";
+import React, { FC, useRef, useState } from "react";
+import * as THREE from "three";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+
+// const deg2rad = (degrees: number) => degrees * (Math.PI / 180);
 
 const Landing: FC = () => {
   return (
     <div>
-      <div className="flex flex-col items-center text-center">
-        <Loading />
-        <h1 className="mt-6">Homepage</h1>
+      <div className="fixed w-screen h-screen">
+        <Canvas>
+          <OrbitControls autoRotate={true} autoRotateSpeed={0.5} />
+          <ambientLight />
+          <pointLight position={[10, 10, 10]} />
+          <Box position={[-3, 0, 0]} />
+          <Box position={[3, 0, 0]} />
+          <Box position={[0, 0, 3]} />
+          <Box position={[0, 0, -3]} />
+        </Canvas>
       </div>
     </div>
+  );
+};
+
+const Box = (props: JSX.IntrinsicElements["mesh"]) => {
+  const ref = useRef<THREE.Mesh>(null!);
+  const [hovered, setHover] = useState(false);
+  const [active, setActive] = useState(false);
+
+  return (
+    <mesh
+      {...props}
+      ref={ref}
+      scale={active ? 1.5 : 1}
+      onClick={(event) => setActive(!active)}
+      onPointerOver={(event) => setHover(true)}
+      onPointerOut={(event) => setHover(false)}
+    >
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={hovered ? "gray" : "white"} />
+    </mesh>
   );
 };
 
